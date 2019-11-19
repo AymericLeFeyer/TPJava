efd03bd99d3957273f1adb8fac4e285d09089a06
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2019-11-19 11:53:00 +0100

    affichage petits arcs

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -20,6 +22,9 @@ public class Main extends Application {
 
     private final int xPos = 50;
     private final int yPos = 50;
+
+    private final Color couleur1 = Color.YELLOW;
+    private final Color couleur2 = Color.ORANGE;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -47,20 +52,54 @@ public class Main extends Application {
     }
 
     public void dessinEnv(Group troupe) {
+        // Initialisation des pions
         Pion.initializePions();
+        // Affichage des lignes
+        Color c = Color.BLACK;
         for (int i = 1; i < 7; i++) {
+            if (i == 1) c = couleur1;
+            else c = Color.BLACK;
             troupe.getChildren().add(new Line(xPos + ecartement,yPos + ecartement*i,xPos + ecartement*6,yPos + ecartement*i));
             troupe.getChildren().add(new Line(xPos + ecartement*i,yPos + ecartement,xPos + ecartement*i,yPos +ecartement*6));
 
+
         }
+        // Affichage des arcs
+        Arc arc1 = new Arc(ecartement + xPos, ecartement + yPos, ecartement , ecartement, 0, 270);
+        arc1.setType(ArcType.OPEN);
+        arc1.setFill(Color.TRANSPARENT);
+        arc1.setStroke(couleur1);
+
+        Arc arc2 = new Arc(ecartement * 6 + xPos, ecartement + yPos, ecartement , ecartement, 270, 270);
+        arc2.setType(ArcType.OPEN);
+        arc2.setFill(Color.TRANSPARENT);
+        arc2.setStroke(couleur1);
+
+        Arc arc3 = new Arc(ecartement + xPos, ecartement * 6 + yPos, ecartement , ecartement, 90, 270);
+        arc3.setType(ArcType.OPEN);
+        arc3.setFill(Color.TRANSPARENT);
+        arc3.setStroke(couleur1);
+
+        Arc arc4 = new Arc(ecartement * 6 + xPos, ecartement * 6 + yPos, ecartement , ecartement, 180, 270);
+        arc4.setType(ArcType.OPEN);
+        arc4.setFill(Color.TRANSPARENT);
+        arc4.setStroke(couleur1);
+
+        troupe.getChildren().add(arc1);
+        troupe.getChildren().add(arc2);
+        troupe.getChildren().add(arc3);
+        troupe.getChildren().add(arc4);
+
+
+
+        // Affichage des ronds blancs
         for (int i = 1; i < 7; i++) {
             for (int j = 1; j < 7; j++) {
                 troupe.getChildren().add(new DessinPion(new Pion(i, j, Color.WHITE), ecartement, radiusPion, xPos, yPos));
 
             }
         }
-
-
+        // Affichage des pions
         for (Pion p : Pion.listPions) {
             DessinPion dp = new DessinPion(p, ecartement, radiusPion, xPos, yPos);
             dessinPions.add(dp);
